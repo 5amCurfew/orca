e2218d6d860f742cc53521849aefc5c857fa5ef1
5amCurfew <samueltobyknight@gmail.com>
2023-12-23 18:05:46 +0000

    begin as gin rest-api

--- main.go
+++ main.go
@@ -1,7 +1,75 @@
 package main
 
-import cmd "github.com/5amCurfew/orca/cmd"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	lib "github.com/5amCurfew/orca/lib"
+	"github.com/gin-gonic/gin"
+)
+
+// curl https://localhost:8080/ping
+
+// curl -X POST \
+// https://localhost:8080/execute \
+// -H 'Content-Type: application/json' \
+// -d '{"file_path": "test.orca"}'
 
 func main() {
-	cmd.Execute()
+	rest := gin.Default()
+
+	rest.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong! 🏓",
+		})
+	})
+
+	rest.POST("/execute", func(c *gin.Context) {
+		var requestData map[string]interface{}
+
+		// Parse JSON request body
+		if err := c.ShouldBindJSON(&requestData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Extract orca file path from the request
+		filePath, ok := requestData["file_path"].(string)
+		if !ok {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "missing orca file_path"})
+			return
+		}
+
+		// Your DAG execution logic here
+		tasks, err := lib.ParseTasks(filePath)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("failed to parse tasks: %s", err)})
+			return
+		}
+
+		g := lib.NewGraph(tasks)
+		g.AddNodes()
+		lib.ParseDependencies(filePath, g)
+		g.CreateTopologicalLayers()
+		jsonData, err := json.MarshalIndent(g, "", "  ")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("failed to marshal JSON: %s", err)})
+			return
+		}
+
+		// Execute DAG
+		lib.ExecuteDAG(g)
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "DAG execution completed",
+			"result":  string(jsonData),
+		})
+	})
+
+	if err := rest.Run(":8080"); err != nil {
+		fmt.Fprintf(os.Stderr, "error starting Gin server: %s", err)
+		os.Exit(1)
+	}
 }
